36b8bf1b57fa951517b4d163b304969e969b4b8c
Hakim Ahmed <126254992+hakimahm@users.noreply.github.com>
2023-02-24 15:08:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $(".navbar").addClass("sticky");
+            $(".gotop").fadeIn();
+        }
+        else{
+            $(".navbar").removeClass("sticky");
+            $(".gotop").fadeOut();
+        }
+    });
+
+    $(".gotop").click(function(){scroll(0,0)});
+    $('.menu-toggler').click(function(){
+        $(this).toggleClass("active");
+        $(".navbar-menu").toggleClass("active");
+    });
+    $(".works").magnificPopup({
+        delegate:'a',
+        type:'image',
+        gallery:{enabled:true}
+    });
+});
